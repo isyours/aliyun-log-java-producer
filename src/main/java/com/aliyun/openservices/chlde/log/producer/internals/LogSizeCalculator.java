bcad4ab81d96236230efa4bd798488eaf30110e0
玉鲲 <haolong.chl@alibaba-inc.com>
2020-01-13 14:39:13 +0800

    update path to avoid conflict.

---
+++ src/main/java/com/aliyun/openservices/chlde/log/producer/internals/LogSizeCalculator.java
@@ -0,0 +1,29 @@
+package com.aliyun.openservices.chlde.log.producer.internals;
+
+import com.aliyun.openservices.log.common.LogContent;
+import com.aliyun.openservices.log.common.LogItem;
+import java.util.List;
+
+public abstract class LogSizeCalculator {
+
+  public static int calculate(LogItem logItem) {
+    int sizeInBytes = 4;
+    for (LogContent content : logItem.GetLogContents()) {
+      if (content.mKey != null) {
+        sizeInBytes += content.mKey.length();
+      }
+      if (content.mValue != null) {
+        sizeInBytes += content.mValue.length();
+      }
+    }
+    return sizeInBytes;
+  }
+
+  public static int calculate(List<LogItem> logItems) {
+    int sizeInBytes = 0;
+    for (LogItem logItem : logItems) {
+      sizeInBytes += calculate(logItem);
+    }
+    return sizeInBytes;
+  }
+}
